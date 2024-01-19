81e30e8b78410f239624dc0df15c6915bf9400d6
GeCrabKani <155706789+GeCrabKani@users.noreply.github.com>
2024-01-19 22:49:51 +0900

    [level 2] Title: N-Queen, Time: 323.88 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.cs
+++ 프로그래머스/2/12952. N－Queen/N－Queen.cs
@@ -1,33 +1,8 @@
 using System;
 
 public class Solution {
-    static int ans = 0;
-    static int[] arr = new int[12];
-
-    static bool check(int level)
-    {
-        for (int i = 0; i < level; i++)
-        {
-            if (arr[i] == arr[level] || Math.Abs(arr[i] - arr[level]) == level - i) return false;
-        }
-        return true;
-    }
-    
-    static void nqueen(int n, int num)
-    {
-        if (num == n) ans++;
-        else
-        {
-            for (int i = 0; i < n; i++)
-            {
-                arr[num] = i;
-                if (check(num)) nqueen(n, num + 1);
-            }
-        }
-    }
-    
     public int solution(int n) {
-        nqueen(n, 0);
-        return ans;
+        int answer = 0;
+        return answer;
     }
 }
