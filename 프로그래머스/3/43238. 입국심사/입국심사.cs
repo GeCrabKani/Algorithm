26ec396fc3835b1b94dd10ac2ae238c1b3d9a4a0
GeCrabKani <155706789+GeCrabKani@users.noreply.github.com>
2024-01-19 23:28:51 +0900

    [level 3] Title: 입국심사, Time: 2.25 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cs
+++ 프로그래머스/3/43238. 입국심사/입국심사.cs
@@ -2,32 +2,7 @@ using System;
 
 public class Solution {
     public long solution(int n, int[] times) {
-        Array.Sort(times);
-        long left = 1;
-        long right = (long)times[times.Length - 1] * n;
-        long mid;
-        long answer = right;
-        while (left <= right)
-        {
-            mid = (left + right) / 2;
-            long people = 0;
-            for (int i = 0; i < times.Length; i++)
-            {
-                people += mid / times[i];
-            }
-            if (people >= n)
-            {
-                if (mid < answer)
-                {
-                    answer = mid;
-                }
-                right = mid - 1;
-            }
-            else
-            {
-                left = mid + 1;
-            }
-        }
+        long answer = 0;
         return answer;
     }
 }
