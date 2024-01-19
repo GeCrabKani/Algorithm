5bb2ee7b824f4cbf94ab508a1a8520211ad641f3
GeCrabKani <155706789+GeCrabKani@users.noreply.github.com>
2024-01-19 19:50:51 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 99.69 ms, Memory: 47.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cs
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cs
@@ -1,41 +1,8 @@
 using System;
-using System.Collections.Generic;
 
 public class Solution {
-    private static string Binary(string n)
-    {
-        string answer = "";
-        int len = n.Length;
-        while (len > 0)
-        {
-            answer = (len % 2).ToString() + answer;
-            len /= 2;
-        }
-        return answer;
-    }
-    
     public int[] solution(string s) {
-        List<int> answer = new List<int>();
-        int count = 0, removeZero = 0;
-        while (s != "1")
-        {
-            string st = "";
-            foreach (char c in s)
-            {
-                if (c == '1')
-                {
-                    st += c;
-                }
-                else
-                {
-                    removeZero++;
-                }
-            }
-            s = Binary(st);
-            count++;
-        }
-        answer.Add(count);
-        answer.Add(removeZero);
-        return answer.ToArray();
+        int[] answer = new int[] {};
+        return answer;
     }
 }
